e62e900231447367f32c92c93bbd42d62323a38c
RemiFELIN <43740654+RemiFELIN@users.noreply.github.com>
2019-02-13 23:29:52 +0100

    Update FelinDiazTestCercleAnime.java

--- java/FelinDiazTestCercleAnime.java
+++ java/FelinDiazTestCercleAnime.java
@@ -3,20 +3,20 @@ import java.awt.*;
 
 public class FelinDiazTestCercleAnime extends Applet{
 
-  private FelinDiazCercleAnime monCercleAnime;
+    private FelinDiazCercleAnime monCercleAnime;
 
-  public void init(){
-    monCercleAnime = new FelinDiazCercleAnime(30, 0);
-  }
+    public void init(){
+        monCercleAnime = new FelinDiazCercleAnime(30, 0);
+    }
 
-  public void paint(Graphics g){  
-    g.drawRect(5, 5, FelinDiazCercleAnime.xMax-5, FelinDiazCercleAnime.yMax-5); 
-    monCercleAnime.dessiner(g, Color.blue);
-    monCercleAnime.avancer(200); 
-    monCercleAnime.dessiner(g, Color.red);
-    monCercleAnime.tourner(90); 
-    monCercleAnime.avancer(100); 
-    monCercleAnime.dessiner(g, Color.green);
-  }
+    public void paint(Graphics g){  
+        g.drawRect(5, 5, FelinDiazCercleAnime.xMax-5, FelinDiazCercleAnime.yMax-5); 
+        monCercleAnime.dessiner(g, Color.blue);
+        monCercleAnime.avancer(200); 
+        monCercleAnime.dessiner(g, Color.red);
+        monCercleAnime.tourner(90); 
+        monCercleAnime.avancer(100); 
+        monCercleAnime.dessiner(g, Color.green);
+    }
   
 }
