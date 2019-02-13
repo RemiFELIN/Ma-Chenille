3ad01fab244d5caee648cfc1d65f2c1ce6b95bf1
RemiFELIN <43740654+RemiFELIN@users.noreply.github.com>
2019-02-13 23:31:05 +0100

    Update FelinDiazTestCercle.java

--- java/FelinDiazTestCercle.java
+++ java/FelinDiazTestCercle.java
@@ -3,19 +3,19 @@ import java.awt.*;
 
 public class FelinDiazTestCercle extends Applet{
 
-  private FelinDiazCercle monCercle;
-  
-  public void init(){
-    monCercle = new FelinDiazCercle(50);
-  }
+    private FelinDiazCercle monCercle;
 
-  public void paint(Graphics g){  
-    g.drawRect(5, 5, FelinDiazCercle.xMax-5, FelinDiazCercle.yMax-5); 
-    monCercle.dessiner(g, Color.blue);
-    monCercle.setDiametre(100); 
-    monCercle.dessiner(g, Color.red);
-    monCercle.deplacerVers(100, 100); 
-    monCercle.dessiner(g, Color.green) ;
-  }
-  
+    public void init(){
+        monCercle = new FelinDiazCercle(50);
+    }
+
+    public void paint(Graphics g){  
+        g.drawRect(5, 5, FelinDiazCercle.xMax-5, FelinDiazCercle.yMax-5); 
+        monCercle.dessiner(g, Color.blue);
+        monCercle.setDiametre(100); 
+        monCercle.dessiner(g, Color.red);
+        monCercle.deplacerVers(100, 100); 
+        monCercle.dessiner(g, Color.green) ;
+    }
+
 }
