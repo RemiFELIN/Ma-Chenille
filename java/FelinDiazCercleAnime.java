95b087b8e7ed2477d9abe0bc4def4f5d3d5e4324
RemiFELIN <43740654+RemiFELIN@users.noreply.github.com>
2019-02-13 22:37:24 +0100

    Update FelinDiazCercleAnime.java

--- java/FelinDiazCercleAnime.java
+++ java/FelinDiazCercleAnime.java
@@ -6,8 +6,8 @@ import java.awt.* ;
  * peut avancer dans la direction de son cap
  * sait si, oui ou non, il est au bord de son espace
  * peut se dessiner en matérialisant son cap 
- * @author (les noms du binome) 
- * @version (a version number or a date)
+ * @author FELIN DIAZ 
+ * @version 1.0
 */
 public class FelinDiazCercleAnime extends FelinDiazCercle {
     
@@ -20,7 +20,10 @@ public class FelinDiazCercleAnime extends FelinDiazCercle {
     * @param diamètre du CercleAnimé
     * @param cap du CercleAnimé
     */
-    public FelinDiazCercleAnime(float x, float y, float d, float cap){super(x,y,d) ; this.cap=cap;}
+    public FelinDiazCercleAnime(float x, float y, float d, float cap){
+        super(x, y, d); 
+        this.cap = cap;
+    }
   
     /************************************************
     * constructor for objects of class CercleAnime
@@ -28,7 +31,10 @@ public class FelinDiazCercleAnime extends FelinDiazCercle {
     * @param diamètre du CercleAnimé
     * @param cap du CercleAnimé
     */
-    public FelinDiazCercleAnime(float d, float cap){super(d) ; this.cap=cap;}
+    public FelinDiazCercleAnime(float d, float cap){
+        super(d); 
+        this.cap = cap;
+    }
     
     /************************************************
     * constructor for objects of class CercleAnime
@@ -36,49 +42,66 @@ public class FelinDiazCercleAnime extends FelinDiazCercle {
     * en fixant son diametre a 0
     * @param cap du CercleAnimé
     */
-    public FelinDiazCercleAnime(float cap){ super(xMax/2, yMax/2, 0); this.cap=cap;}
+    
+    public FelinDiazCercleAnime(float cap){ 
+        super(xMax/2, yMax/2, 0); 
+        this.cap = cap;
+    }
     
     /************************************************
     * constructor for objects of class CercleAnime
     * créer un CercleAnime au centre de la fenêtre
     * en fixant son diamètre à 5 et son cap a 0
     */
-    public FelinDiazCercleAnime(){ super(xMax/2, yMax/2, 5) ; this.cap=0;}
+    
+    public FelinDiazCercleAnime(){ 
+        super(xMax/2, yMax/2, 5); 
+        this.cap = 0;
+    }
    
     /************************************************
     * obtenir le cap
     * @return cap
     */
-    public float getCap(){return this.cap;}
+    
+    public float getCap(){
+        return this.cap;
+    }
     
     /************************************************
     * un CercleAnime sait s'il est bord de son espace 
     * @param les bords max de l'espace 
     * @return suis-je au bord ?
     */
+    
     public boolean auBord(float xMax, float yMax){
-      return (this.getAbscisse()+this.getDiametre()>xMax-10 || 
-              this.getAbscisse()-this.getDiametre()<10      ||
-              this.getOrdonnee()+this.getDiametre()>yMax-10 ||
-              this.getOrdonnee()-this.getDiametre()<10);
+        return (this.getAbscisse()+this.getDiametre()>xMax-10 || 
+                this.getAbscisse()-this.getDiametre()<10      ||
+                this.getOrdonnee()+this.getDiametre()>yMax-10 ||
+                this.getOrdonnee()-this.getDiametre()<10      );
     }
     
     /************************************************
     * un CercleAnime peut tourner d'un certain angle 
     * @param deltaCap incrément du cap
     */
-    public void tourner(float deltaCap){ this.cap = this.cap + deltaCap; }
+    
+    public void tourner(float deltaCap){ 
+        this.cap = this.cap + deltaCap; 
+    }
     
     /************************************************
     * un CercleAnime peut avancer selon son cap
     * s'il est au bord de son espace, il fait demi-tour
     * @param longueur du déplacement
     */
+    
     public void avancer(float longueur){
-       if (this.auBord(xMax, yMax)) this.tourner(180);
-       float capEnRadian=this.cap*(float)(Math.PI/180); // conversion degre2radian
-       this.setAbscisse(getAbscisse() + longueur*(float) Math.cos(capEnRadian));
-       this.setOrdonnee(getOrdonnee() + longueur*(float) Math.sin(capEnRadian));
+        if (this.auBord(xMax, yMax)) 
+            this.tourner(180);
+        float capEnRadian = this.cap*(float)(Math.PI/180); // conversion degre2radian
+        this.setAbscisse(getAbscisse() + longueur*(float) Math.cos(capEnRadian));
+        this.setOrdonnee(getOrdonnee() + longueur*(float) Math.sin(capEnRadian));
     }
    
     /************************************************
@@ -87,15 +110,16 @@ public class FelinDiazCercleAnime extends FelinDiazCercle {
     * @param le contexte graphique
     * @param la couleur
     */
+    
     private void dessinerCap(Graphics g, Color c){
         float capEnRadian=this.cap*(float)(Math.PI/180);
         g.drawLine(
-                   Math.round(this.getAbscisse()),
-                   Math.round(this.getOrdonnee()),
-                   Math.round(this.getAbscisse()+this.getDiametre()*(float) Math.cos(capEnRadian)),
-                   Math.round(this.getOrdonnee()+this.getDiametre()*(float) Math.sin(capEnRadian))
-                   );
-    }
+               Math.round(this.getAbscisse()),
+               Math.round(this.getOrdonnee()),
+               Math.round(this.getAbscisse()+this.getDiametre()*(float) Math.cos(capEnRadian)),
+               Math.round(this.getOrdonnee()+this.getDiametre()*(float) Math.sin(capEnRadian))
+               );
+    }    
     
     /************************************************
     * un CercleAnime peut se dessiner d'une certaine couleur
@@ -103,7 +127,7 @@ public class FelinDiazCercleAnime extends FelinDiazCercle {
     * @param la couleur
     */
     public void dessiner(Graphics g, Color c){ // redéfinition d'une méthode de la super classe
-        super.dessiner(g,c) ; // dessiner le cercle
-        this.dessinerCap(g,c) ; // dessiner le cap
+        super.dessiner(g,c); // dessiner le cercle
+        this.dessinerCap(g,c); // dessiner le cap
     }
 }
